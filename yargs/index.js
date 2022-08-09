d667144c82079146397d06864c3458b29b342b52
franciscofassi97 <fassifrancisco9716@gmail.com>
2022-08-09 19:27:16 -0300

    add parameter modo to Yargs

--- yargs/index.js
+++ yargs/index.js
@@ -3,8 +3,10 @@ const yargs = require('yargs');
 const argumentos = process.argv.slice(2);
 const parsear = yargs(argumentos).default({
   port: 8080,
+  modo: "FORK",
 }).alias({
   p: 'port',
+  m: 'modo',
 }).argv;
 
 module.exports = parsear;
